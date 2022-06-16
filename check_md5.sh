01ee62057a48a9d198fa55c58211b7c035c3df69
Savin Sergey <103108972+rtssv@users.noreply.github.com>
2022-06-16 20:11:35 +0300

    Update check_md5.sh

--- check_md5.sh
+++ check_md5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 online_md5="$(curl -sL http://51.250.45.201:9889 | md5sum | cut -d ' ' -f 1)"
-local_md5="$(md5sum index.html | cut -d ' ' -f 1)"
+olocal_md5="$(md5sum index.html | cut -d ' ' -f 1)"
 if [ "$online_md5" = "$local_md5" ]; then
     echo "Ok"
 else
